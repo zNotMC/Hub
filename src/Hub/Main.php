5c57b5020f2337f98ebe560aef353ccbce8df995
zNotMC <87515497+zNotMC@users.noreply.github.com>
2022-02-08 18:41:24 -0600

    Update Main.php

--- src/Hub/Main.php
+++ src/Hub/Main.php
@@ -4,19 +4,42 @@
 namespace Hub;
 
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\event\Listener;
-
 use pocketmine\plugin\{PluginOwned, PluginOwnedTrait};
-
 use pocketmine\command\utils\InvalidCommandSyntaxException;
-
 use pocketmine\Server;
-
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
 
-    
-
 class Main extends PluginBase implements Listener {
+
+protected static $instance;
+
+public function onLoad() : void {
+        self::$instance = $this;
+    }
+
+public function onEnable() : void {
+        $server = $this->getServer();
+        $this->getLogger()->info("
+██╗░░██╗██╗░░░██╗██████╗░
+██║░░██║██║░░░██║██╔══██╗
+███████║██║░░░██║██████╦╝
+██╔══██║██║░░░██║██╔══██╗
+██║░░██║╚██████╔╝██████╦╝
+╚═╝░░╚═╝░╚═════╝░╚═════╝░
+");
+        $name = $this->getConfig()->get("server_name");
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+        $this->getServer()->getNetwork()->setName($name);
+    }
+    
+    public function onDisable() : void {
+        $this->getLogger()->info("Hub Off");
+    }
+    
+    public static function getInstance() : Main {
+        return self::$instance;
+    }
+}
 
 ?>
