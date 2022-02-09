92a3432f259623891a4c4dfcbd522f18a50b9fb3
zNotMC <87515497+zNotMC@users.noreply.github.com>
2022-02-08 18:39:18 -0600

    Update Main.php

--- src/Hub/Main.php
+++ src/Hub/Main.php
@@ -3,4 +3,20 @@
 
 namespace Hub;
 
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+
+use pocketmine\plugin\{PluginOwned, PluginOwnedTrait};
+
+use pocketmine\command\utils\InvalidCommandSyntaxException;
+
+use pocketmine\Server;
+
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+    
+
+class Main extends PluginBase implements Listener {
+
 ?>
